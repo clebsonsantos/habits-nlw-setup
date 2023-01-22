adbf9ea488f34a1327771e5688e440126e658212
Clebson Santos <clebsonsantosmtt@gmail.com>
2023-01-22 11:07:25 -0300

    chore: create form to create a new Habit

--- mobile/src/components/HabitDay.tsx
+++ mobile/src/components/HabitDay.tsx
@@ -1,4 +1,4 @@
-import { TouchableOpacity, Dimensions } from "react-native";
+import { TouchableOpacity, Dimensions, TouchableOpacityProps } from "react-native";
 
 const weekDays = 7;
 const screenHorizontalPadding = (32 * 2) /5
@@ -6,12 +6,16 @@ const screenHorizontalPadding = (32 * 2) /5
 export const dayMarginBetween = 8
 export const daySize = (Dimensions.get("screen").width / weekDays) - (screenHorizontalPadding + 5)
 
-export function HabitDay(){
+interface Props extends TouchableOpacityProps {
+
+}
+export function HabitDay({ ...rest }: Props){
   return (
     <TouchableOpacity 
       className="bg-zinc-900 rounded-lg border-2 m-1 border-zinc-800"
       activeOpacity={0.7}
       style={{width: daySize, height: daySize}}
+      { ...rest }
     />
   )
 }
--- mobile/src/screens/Home.tsx
+++ mobile/src/screens/Home.tsx
@@ -1,3 +1,4 @@
+import { useNavigation } from "@react-navigation/native";
 import { Text, View, ScrollView } from "react-native";
 import { HabitDay, daySize } from "../components/HabitDay";
 import { Header } from "../components/Header";
@@ -8,6 +9,7 @@ const minimunSummaryDatesSize = 18 * 5;
 const amountOfDaysToFill = minimunSummaryDatesSize - datesFromYearStart.length
 
 export function Home(){
+  const { navigate } = useNavigation()
   return (
     <View className="flex-1 bg-background px-8 pt-16">
       <Header />
@@ -29,7 +31,7 @@ export function Home(){
         <View className="flex-row flex-wrap">
           {
             datesFromYearStart.map(date => (
-              <HabitDay key={date.toISOString()}/>
+              <HabitDay key={date.toISOString()} onPress={() => navigate('habit', { date: date.toISOString() })}/>
             ))
           }
 
