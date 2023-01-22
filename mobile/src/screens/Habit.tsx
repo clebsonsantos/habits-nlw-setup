a602c895fc95a92e1497ae8c00baa16e5b970b78
Clebson Santos <clebsonsantosmtt@gmail.com>
2023-01-22 11:45:11 -0300

    chore: create screen to habit selected

--- mobile/src/screens/Habit.tsx
+++ mobile/src/screens/Habit.tsx
@@ -1,9 +1,84 @@
-import { View } from "react-native";
+import { useRoute } from "@react-navigation/native";
+import { Alert, ScrollView, Text, View } from "react-native";
+import { BackButton } from "../components/BackButton";
+import dayjs from "dayjs";
+import { ProgressBar } from "../components/ProgressBar";
+import { useState } from "react";
+import { CheckBox } from "../components/ChackBox";
+import clsx from "clsx";
+import { HabitsEmpty } from "../components/HabitsEmpty";
+
+interface Params {
+  date: string
+}
+interface DayInfoProps {
+  completedHabits: string[];
+  possibleHabits: {
+    id: string;
+    title: string;
+  }[];
+}
 
 export function Habit () {
+  const [loading, setLoading] = useState(true);
+  const [dayInfo, setDayInfo] = useState<DayInfoProps | null>(null);
+  const [completedHabits, setCompletedHabits] = useState<string[]>([])
+
+  const route = useRoute()
+  const { date } = route.params as Params
+  const parsedDate = dayjs(date)
+  const isDateInPast = parsedDate.endOf('day').isBefore(new Date());
+  const dayOfWeek = parsedDate.format('dddd')
+  const dayAndMonth = parsedDate.format("DD/MM")
+
+  async function handleToggleHabits(habitId: string) {
+    try {
+      // await api.patch(`/habits/${habitId}/toggle`);
+
+      if (completedHabits?.includes(habitId)) {
+        setCompletedHabits(prevState => prevState.filter(habit => habit !== habitId));
+      } else {
+        setCompletedHabits(prevState => [...prevState, habitId]);
+      }
+    } catch (error) {
+      console.log(error)
+      Alert.alert('Ops', 'Não foi possível atualizar o status do hábito.')
+    }
+  }
+
   return (
     <View className="flex-1 bg-background px-8 pt-16">
-
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ paddingBottom: 100 }}
+      >
+        <BackButton />
+        <Text className="mt-6 text-zinc-400 font-semibold text-base lowercase">
+          { dayOfWeek }
+        </Text>
+        <Text className="text-white font-extrabold text-3xl">
+          {dayAndMonth}
+        </Text>
+        <ProgressBar progress={20}/>
+        <View className={clsx("mt-6", {
+            ['opacity-50']: isDateInPast
+          })}>
+          {
+            dayInfo?.possibleHabits ?
+            dayInfo.possibleHabits?.map(habit => (
+              <CheckBox 
+                key={habit.id}
+                title={habit.title}
+                checked={completedHabits?.includes(habit.id)}
+                onPress={() => handleToggleHabits(habit.id)}
+                disabled={isDateInPast}
+              />
+            ))
+            : 
+            <HabitsEmpty />
+          }
+        </View>
+      </ScrollView>
     </View>
   )
 }
